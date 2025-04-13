7ff04d556727838c68fdd607f7659ee3de054e0e
Umran Terece <chost609@gmail.com>
2025-04-14 02:30:09 +0300

    Urunlerin Listelenmesi ve Pagination

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Application.RequestParameters;
 using ETicaretAPI.Application.ViewModels.Products;
 using ETicaretAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
@@ -22,9 +23,23 @@ namespace ETicaretAPI.API.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get([FromQuery]Pagination pagination)
         {
-            return Ok(_productReadRepository.GetAll(false));
+            var totalCount = _productReadRepository.GetAll(false).Count();
+            var products= _productReadRepository.GetAll(false).Select(p=>new
+            {
+                p.Id,
+                p.Name,
+                p.Stock,
+                p.Price,
+                p.CreatedDate,
+                p.UpdatedDate
+            }).Skip(pagination.Page*pagination.Size).Take(pagination.Size);
+            
+            return Ok(new
+            {
+                totalCount,products
+            });
             //bu yorum satiri rider uzerinden yapilmistir test
         }
 
@@ -37,11 +52,6 @@ namespace ETicaretAPI.API.Controllers
         [HttpPost]
         public async Task<IActionResult> Post(VM_Create_Product model)
         {
-            if (ModelState.IsValid)
-            {
-               
-            }
-            
             await _productWriteRepository.AddAsync(new()
             {
                 Name = model.Name,
